3d2c1d379f01cc3b01744505fa038a51365e7bc5
rajendra <akrajendra597@.com>
2025-11-19 12:25:02 +0530

    expense -shell

---
+++ backend.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Disabling exiting default nodejs"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing nodejs"
+
+id expense 
+if [ $? -ne 0 ]
+then
+   useradd expense
+   VALIDATE $? "Sdding expense user"
+
+else
+   echo -e "expense usern alredy exists ...$Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOG_FILE_NAME
+VALIDATE $? "crating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Dowloading backend"
+
+cd /app
+rm -rf /app/*
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzip backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service 
+
+# Prepare Mysql Schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installig Mysql Client"
+
+mysql -h mysql.rvbp.store -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the transations schema and tables"
+
+systemctl darmon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Daemon Reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? " Enabling backend"
+
+systemctl restart ackend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Backend"
